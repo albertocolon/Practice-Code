0126aedc07b665a45ebcd85f066eaf35fc195661
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-09-30 20:25:49 -0400

    Just adding the instructions to the lab07 to confirm something.

---
+++ lab07instructions.cpp
@@ -0,0 +1,54 @@
+/*
+* CECS 2203, Computer Programming I Lab
+* Fall 2021, Sec. 04
+* Date: September 30, 2021
+* Topic Lab 7 - Two Dimensional Arrays
+* File name: lab07.cpp
+* YOUR NAME, YOUR ID #
+*/
+#include <iostream>
+using namespace std;
+// Global area & function prototypes
+// declare an integer constant named COLUMNS and initialize to 3
+
+void getValues(int[][???],int);
+void printArray(int[][???],int);
+void personalInfo();
+
+int main(){
+	// declare an integer constant named ROWS and initialize to 3
+
+	// declare a two-dimensional intege array named twoDim
+	// use the two constants respectively for the array's size
+  // initialize all array elements to 0
+
+	// call the method getValues and enter  the following values
+	// 25, 85, 1, 36, 100, 3, 45, 76, 22
+
+	// cal the method to print the array's values
+
+	// call the method to print your personal info
+
+	system("pause");
+	return 0;
+}
+
+// sets the value for each element in the two-dimensional array
+// by prompting the user for a value
+// For the prompt, use the phrase:
+// "Enter a whole number: ";
+// Store the value in the array using a nested for iteration control structure
+void getValues(int[][???],int,){
+
+}
+
+// prints the contents of the array in a table-like manner
+// Starts with the phrase "The contents of the array are:\n"
+// set so each value is printed aligned to the right in a 5 space field
+void printArray(int[][???],int){}
+
+// this method prints the phrase
+// "Program developed by [YOUR NAME], ID#[YOUR ID NUMBER]"
+// where the square brackets and the text within is substitued with
+// your personal information.
+void personalInfo(){}
